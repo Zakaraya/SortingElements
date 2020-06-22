5ff56a81830d70241d3e79fe49927a52aa62cd6f
Борис Закарая <zakaraya2000@mail.ru>
2020-06-22 10:36:26 +0300

    Добавьте файлы проекта.

---
+++ SortingElements/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace SortingElements
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.Chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.ListOutput = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Chart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Chart
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.Chart.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.Chart.Legends.Add(legend2);
+            this.Chart.Location = new System.Drawing.Point(161, 18);
+            this.Chart.Name = "Chart";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.Chart.Series.Add(series2);
+            this.Chart.Size = new System.Drawing.Size(300, 300);
+            this.Chart.TabIndex = 9;
+            this.Chart.Text = "chart2";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 244);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 74);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Анализ поиска";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // ListOutput
+            // 
+            this.ListOutput.FormattingEnabled = true;
+            this.ListOutput.Location = new System.Drawing.Point(12, 12);
+            this.ListOutput.Name = "ListOutput";
+            this.ListOutput.Size = new System.Drawing.Size(120, 199);
+            this.ListOutput.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(473, 368);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ListOutput);
+            this.Controls.Add(this.Chart);
+            this.Name = "Form1";
+            this.Text = "Sorting";
+            ((System.ComponentModel.ISupportInitialize)(this.Chart)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart Chart;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox ListOutput;
+    }
+}
+
---
+++ SortingElements/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace SortingElements
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<int> ListElements = new List<int>(9000000);
+        Dictionary<int, int> DictionaryElements = new Dictionary<int, int>(9000000);
+        int[] ArrayElements = new int[9000000];
+
+        public List<int> CreateList()
+        {
+            for (int i = 0; i < 9000000; i++)
+            {
+                ListElements.Add(i);
+            }
+            return ListElements;
+        }
+
+        //в списке в листе использовать contains
+        //в массиве array и его метод indexof
+        //в словаре через containskey 
+        public Dictionary<int, int> CreateDictionary()
+        {
+            Dictionary<int, int> newdictionary = new Dictionary<int, int>(9000000);
+            for (int i = 0; i < 9000000; i++)
+            {
+                newdictionary.Add(i, i + 1);
+            }
+            return DictionaryElements;
+        }
+
+        public int[] CreateMas()
+        {
+            for (int i = 0; i < 9000000; i++)
+            {
+                ArrayElements[i] = i;
+            }
+            return ArrayElements;
+        }
+
+        public double SearchInList(List<int> List, int El)
+        {
+            Stopwatch Time = new Stopwatch();
+            Time.Start();
+            List.Contains(El);
+            Time.Stop();
+            return Time.Elapsed.TotalMilliseconds;
+        }
+
+        public double SearchInDict(Dictionary<int, int> Dictionary, int El)
+        {
+            Stopwatch Time = new Stopwatch();
+            Time.Start();
+            Dictionary.ContainsKey(El);
+            Time.Stop();
+            return Time.Elapsed.TotalMilliseconds;
+        }
+        public double SearchInMas(int[] Mas, int El)
+        {
+            Stopwatch Time = new Stopwatch();
+            Time.Start();
+            Array.IndexOf(Mas, El);
+            Time.Stop();
+            return Time.Elapsed.TotalMilliseconds;
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            ListOutput.Items.Clear();
+            Random random = new Random();
+            int Element = random.Next(0, 9000000);
+            for (int i = 0; i < 90000; i++)
+            {
+                Element = i;
+            }
+            List<int> List = CreateList();
+            //  Dictionary<int, int> Dictionary = CreateDictionary();
+            int[] Array = CreateMas();
+            Chart.Series[0].Points.Clear();
+            ListOutput.Items.Insert(0, "List - " + SearchInList(List, Element).ToString());
+            ListOutput.Items.Insert(1, "Dictionary - " + SearchInDict(DictionaryElements, Element).ToString());
+            ListOutput.Items.Insert(2, "Array - " + SearchInMas(Array, Element).ToString());
+            Chart.Series[0].Points.AddXY("List", SearchInList(List, Element).ToString());
+            Chart.Series[0].Points.AddXY("Dictionary", SearchInDict(DictionaryElements, Element).ToString());
+            Chart.Series[0].Points.AddXY("Array", SearchInMas(Array, Element).ToString());
+        }
+    }
+}
+
+
+
